7f8bc7bd35da82586f52933260999645c8810e04
Darkey <148985917+toolsdark@users.noreply.github.com>
2023-10-27 03:52:43 -0300

    Create mailer.php

---
+++ mailer.php
@@ -0,0 +1,24 @@
+<?php
+
+// This is just a skeleton layout, to get whole source code or license contact me.
+
+$validLicenseKey = 'Contact me via telegram to get license key--> @toolsdark';
+
+// Get the user's input (license key)
+$userLicenseKey = isset($_POST['license_key']) ? $_POST['license_key'] : '';
+
+// Check if the provided license key is valid
+if ($userLicenseKey === $validLicenseKey) {
+
+  //Whole script will be available to you  after a successful purchase !!
+
+    if ($mail_sent) {
+        echo "Email Sent Successfully!";
+    } else {
+        echo "Failed to Send Email!";
+    }
+} else {
+    // Invalid license key, take appropriate action (e.g., deny access)
+    echo "Invalid license key. Access denied.";
+}
+?>
